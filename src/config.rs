93c00d4fe2c71dffcad3cd20ea74219d846f9410
Brando <fong.m.brandon97@gmail.com>
2023-07-27 15:36:29 -0700

    Saving work

--- src/config.rs
+++ src/config.rs
@@ -4,18 +4,40 @@
  */
 
 use std::fs::File;
+use std::io::BufReader;
+use std::io::BufRead;
+use crate::keypath::KeyPath;
+use std::io::Lines;
 
 pub struct Config {
     _path: String,
-    _file: File
+    _reader: Option<BufReader<File>>
 }
 
 impl Config {
     pub fn open_for_read(path: &str) -> Result<Self, &str> {
-        let result = Self { _path: path.to_string(), _file: None};
+        let mut result = Self {_path: path.to_string(), _reader: None };
         
+        match File::options().read(true).open(&result._path) {
+            Err(e) => {
+                eprintln!("Error: {}", e);
+                return Err("Could not open for read");
+            } Ok(f) => {
+                result._reader = Some(BufReader::new(f));
+            }
+        }
 
-        Ok(result);
+        if result._reader.is_none() {
+            return Err("Reader could not be used");
+        }
+
+        Ok(result)
+    }
+
+    pub fn entries(&self) -> Result<KeyPath, &str> {
+        let result = KeyPath::new("", "");
+
+        return Ok(result);
     }
 }
 
