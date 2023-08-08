3ce63efc560acbc18edd381e413c745b13a485da
Brandon Fong <55638760+BrandonMFong@users.noreply.github.com>
2023-08-08 14:45:16 -0700

    Create a Config class that handles io for keypath config file (#5)

---
+++ src/config.rs
@@ -0,0 +1,243 @@
+/**
+ * author: Brando
+ * date: 7/27/23
+ */
+
+use std::fs::File;
+use std::io::BufReader;
+use std::io::BufRead;
+use crate::keypath::KeyPath;
+use crate::path_exists;
+use std::io::Lines;
+use std::io;
+use std::fs::OpenOptions;
+use std::io::Write;
+use std::io::SeekFrom;
+use std::io::Seek;
+
+pub struct Config {
+    _path: String,
+}
+
+impl Config {
+    /**
+     * Creates a new config object
+     */
+    pub fn new(path: &str) -> Result<Self, &str> {
+        if path.is_empty() {
+            return Err("empty path to config");
+        } else {
+            return Ok(Self{ _path: path.to_string().clone()});
+        }
+    }
+
+    /**
+     * returns an iterator that provides each key path entry in config
+     */
+    pub fn entries(&self) -> Entries {
+        match Self::create_reader(&self._path) {
+            Err(_) => {
+                return Entries{ _lines: None };
+            } Ok(f) => {
+                return Entries{ _lines: Some(f.lines()) };
+            }
+        }
+    }
+
+    /**
+     * Writes keypath entry into config
+     *
+     * does not check if kp already exists
+     */
+    pub fn enter_keypath(&self, kp: KeyPath) -> Result<(), &str> {
+        // Write new key and path pair
+        let file_writer = Config::create_writer(&self._path);
+        if file_writer.is_err() {
+            return Err(file_writer.err().unwrap());
+        }
+
+        // write
+        if let Err(error) = writeln!(file_writer.as_ref().unwrap(), "{}", kp.entry()) {
+            eprintln!("Error occurred writing line: {} ({})", kp.entry(), error);
+            return Err("could not write entry");
+        }
+
+        // Make sure the writer flushed all data
+        if let Err(error) = file_writer.as_ref().unwrap().flush() {
+            eprintln!("{}", error);
+            return Err("error flushing");
+        }
+
+        return Ok(());
+    }
+
+    /**
+     * removes entry that has 'key'
+     */
+    pub fn remove_keypath(&self, key: &str) -> Result<(), &str> {
+        // Open the file in read-write mode
+        let file = Config::create_writer(&self._path);
+        if file.is_err() {
+            return Err(file.err().unwrap());
+        }
+
+        // Create a buffer to store the modified contents
+        let mut buffer = Vec::new();
+
+        // Seek to the beginning of the file
+        if let Err(error) = file.as_ref().unwrap().seek(SeekFrom::Start(0)) {
+            eprintln!("Error occured: {}", error);
+            return Err("couldn't seek to start of file");
+        }
+
+        // Iterate over the lines and exclude the line to be removed
+        for line in io::BufReader::new(file.as_ref().unwrap()).lines() {
+            // Write non-matching lines to the buffer
+            if let Ok(ref ip) = line {
+                let key_path_pair = KeyPath::from_entry(&ip);
+                if key_path_pair.is_valid() && key_path_pair.key() != key {
+                    buffer.extend(line.unwrap().bytes());
+                    buffer.push(b'\n');
+                }
+            }
+        }
+
+        // Truncate the file to the current position (i.e., remove the remaining contents)
+        let seek_current = file.as_ref().unwrap().seek(SeekFrom::Start(0)).unwrap();
+        if let Err(error) = file.as_ref().unwrap().set_len(seek_current) {
+            eprintln!("Error occured: {}", error);
+            return Err("couldn't erase file");
+        }
+
+        // Write the modified contents back to the file
+        if let Err(error) = file.as_ref().unwrap().write_all(&buffer) {
+            eprintln!("Error occured: {}", error);
+            return Err("couldn't dump to file");
+        }
+
+        return Ok(());
+    }
+
+    /**
+     * creates a writer for path
+     */
+    fn create_writer(path: &str) -> Result<File, &str> {
+        match OpenOptions::new().create(true).write(true).append(true).open(&path) {
+            Err(_) => {
+                Err("couldn't open for writing")
+            } Ok(res) => {
+                Ok(res)
+            }
+        }
+    }
+
+    /**
+     * creates a reader object for path
+     */
+    fn create_reader(path: &str) -> Result<BufReader<File>, &str> {
+        if !path_exists(&path) {
+            Err("cannot create a reader for a file that does not exist")
+        } else {
+            match File::open(path) {
+                Err(e) => {
+                    eprintln!("file cannot be open: {}", e);
+                    Err("could not open file")
+                } Ok(file) => {
+                    let reader = BufReader::new(file);
+                    Ok(reader)
+                }
+            }
+        }
+    }
+}
+
+pub struct Entries {
+    _lines: Option<Lines<BufReader<File>>>
+}
+
+// https://doc.rust-lang.org/rust-by-example/trait/iter.html
+impl Iterator for Entries {
+    type Item = KeyPath;
+    fn next(&mut self) -> Option<Self::Item> {
+        if self._lines.is_none() {
+            None
+        } else {
+            let lines = &mut self._lines;
+            match lines.as_mut().unwrap().next() {
+                Some(entry) => return Some(KeyPath::from_entry(&entry.unwrap())),
+                None => return None,
+            }
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::tests::{setup, teardown};
+    use crate::{goto_key_paths_file_path, goto_utils_path, path_exists};
+
+    #[test]
+    fn valid_file_reader() {
+        setup();
+        let path = goto_key_paths_file_path();
+        let reader = Config::create_reader(&path);
+        assert!(reader.is_ok());
+        assert!(reader.unwrap().lines().count() == 1, "we are expecting only one line in this test case");
+        teardown();
+    }
+
+    #[test]
+    fn config_constructor() {
+        setup();
+        let path = goto_key_paths_file_path();
+        let conf = Config::new(&path);
+        assert!(conf.is_ok());
+        teardown();
+    }
+
+    #[test]
+    fn entries_with_no_lines() {
+        let entries = Entries { _lines: None };
+        assert!(entries._lines.is_none());
+    }
+
+    #[test]
+    fn config_constructor_with_empty_path() {
+        let conf = Config::new("");
+        assert!(conf.is_err());
+    }
+
+    #[test]
+    fn loop_through_test_entries() {
+        setup();
+        let path = goto_key_paths_file_path();
+        let conf = Config::new(&path);
+        assert!(conf.is_ok());
+        let mut i = 0;
+        for _ in conf.unwrap().entries() {
+            i += 1;
+        }
+
+        assert!(i > 0);
+        teardown();
+    }
+
+    #[test]
+    fn create_writer() {
+        setup();
+        let path = goto_key_paths_file_path();
+        let writer = Config::create_writer(&path);
+        assert!(writer.is_ok());
+
+        // See if file is created if it doesn't exist
+        let mut path = goto_utils_path();
+        println!("{}", path);
+        path += "/test";
+        let writer = Config::create_writer(&path);
+        assert!(writer.is_ok());
+        assert!(path_exists(&path));
+        teardown();
+    }
+}
+
--- src/main.rs
+++ src/main.rs
@@ -4,19 +4,17 @@
  */
 
 mod keypath;
+mod config;
 
 use std::env;
 use std::process;
 use home;
 use std::path::PathBuf;
-use std::fs::File;
-use std::io::{self, BufRead, BufReader};
 use std::path::Path;
-use std::fs::OpenOptions;
-use std::io::{prelude::*};
 use std::fs::canonicalize;
-use std::io::SeekFrom;
 use crate::keypath::KeyPath;
+use crate::config::Config;
+use std::fs;
 
 static ARG_GETPATH: &'static str = "getpath";
 static ARG_GETKEYS: &'static str = "getkeys";
@@ -62,6 +60,7 @@ fn main() {
     if args[1].eq(ARG_HELP) {
         help();
     } else {
+        // TODO: passing 'add' wont show a good error log
         if args.len() > 3 {
             if args[1].eq(ARG_ADD) {
                 error = add_key_path(&args[2], &args[3]);
@@ -91,48 +90,38 @@ fn main() {
 }
 
 fn print_all_key_pairs() -> i32 {
-    match get_file_reader_for_file(&goto_key_paths_file_path()) {
+    match Config::new(&goto_key_paths_file_path()) {
         Err(e) => {
-            eprintln!("Could not read file {}: {}", goto_key_paths_file_path(), e);
+            eprintln!("{}", e);
             return -1;
-        } Ok(reader) => {
-            for line in reader.lines() {
-                if let Ok(ip) = line {
-                    // key|path
-                    let key_path_pair = KeyPath::from_entry(&ip);
-                    if !key_path_pair.is_valid() {
-                        eprintln!("error in key path pair");
-                        return -1;
-                    } else {
-                        println!("{} => {}", key_path_pair.key(), key_path_pair.path());
-                    }
+        } Ok(conf) => {
+            for key_path_pair in conf.entries() {
+                if !key_path_pair.is_valid() {
+                    eprintln!("error in key path pair");
+                    return -1;
+                } else {
+                    println!("{} => {}", key_path_pair.key(), key_path_pair.path());
                 }
             }
         }
     }
 
     return 0;
-
 }
 
 fn print_path_for_key(key: &String) -> i32 {
-    // See if key already exists
-    match get_file_reader_for_file(&goto_key_paths_file_path()) {
+    match Config::new(&goto_key_paths_file_path()) {
         Err(e) => {
             eprintln!("Could not read file {}: {}", goto_key_paths_file_path(), e);
             return -1;
-        } Ok(reader) => {
-            for line in reader.lines() {
-                if let Ok(ip) = line {
-                    // key|path
-                    let key_path_pair = KeyPath::from_entry(&ip);
-                    if !key_path_pair.is_valid() {
-                        eprintln!("error in key path pair");
-                        return -1;
-                    } else if key_path_pair.key() == key {
-                        println!("{}", key_path_pair.path());
-                        return 0;
-                    }
+        } Ok(conf) => {
+            for key_path_pair in conf.entries() {
+                if !key_path_pair.is_valid() {
+                    eprintln!("error in key path pair");
+                    return -1;
+                } else if key_path_pair.key() == key {
+                    println!("{}", key_path_pair.path());
+                    return 0;
                 }
             }
         }
@@ -151,22 +140,17 @@ fn print_keys_for_path(path: &String) -> i32 {
 
     // Expand the input path
     let expanded_path = expand_path(&path);
-
-    match get_file_reader_for_file(&goto_key_paths_file_path()) {
+    match Config::new(&goto_key_paths_file_path()) {
         Err(e) => {
             eprintln!("Could not read file {}: {}", goto_key_paths_file_path(), e);
             return -1;
-        } Ok(reader) => {
-            for line in reader.lines() {
-                if let Ok(ip) = line {
-                    // key|path
-                    let key_path_pair = KeyPath::from_entry(&ip);
-                    if !key_path_pair.is_valid() {
-                        eprintln!("error in key path pair");
-                        return -1;
-                    } else if key_path_pair.path() == expanded_path {
-                        println!("{} => {}", key_path_pair.key(), key_path_pair.path());
-                    }
+        } Ok(conf) => {
+            for key_path_pair in conf.entries() {
+                if !key_path_pair.is_valid() {
+                    eprintln!("error in key path pair");
+                    return -1;
+                } else if key_path_pair.path() == expanded_path {
+                    println!("{} => {}", key_path_pair.key(), key_path_pair.path());
                 }
             }
         }
@@ -180,21 +164,17 @@ fn print_keys_for_path(path: &String) -> i32 {
  */
 fn print_suggested_keys(input: &String) -> i32 {
     // Expand the input path
-    match get_file_reader_for_file(&goto_key_paths_file_path()) {
+    match Config::new(&goto_key_paths_file_path()) {
         Err(e) => {
             eprintln!("Could not read file {}: {}", goto_key_paths_file_path(), e);
             return -1;
-        } Ok(reader) => {
-            for line in reader.lines() {
-                if let Ok(ip) = line {
-                    // key|path
-                    let key_path_pair = KeyPath::from_entry(&ip);
-                    if !key_path_pair.is_valid() {
-                        eprintln!("error in key path pair");
-                        return -1;
-                    } else if key_path_pair.key().starts_with(input) {
-                        println!("{}", key_path_pair.key());
-                    }
+        } Ok(conf) => {
+            for key_path_pair in conf.entries() {
+                if !key_path_pair.is_valid() {
+                    eprintln!("error in key path pair");
+                    return -1;
+                } else if key_path_pair.key().starts_with(input) {
+                    println!("{}", key_path_pair.key());
                 }
             }
         }
@@ -204,99 +184,60 @@ fn print_suggested_keys(input: &String) -> i32 {
 }
 
 fn remove_key_path(key: &String) -> i32 {
-    // Open the file in read-write mode
-    let mut file = OpenOptions::new().read(true).write(true).open(&goto_key_paths_file_path()).unwrap();
-
-    // Create a buffer to store the modified contents
-    let mut buffer = Vec::new();
-
-    // Seek to the beginning of the file
-    if let Err(error) = file.seek(SeekFrom::Start(0)) {
-        eprintln!("Error occured: {}", error);
+    let key_paths_file_path = goto_key_paths_file_path();
+    let config = Config::new(&key_paths_file_path);
+    if let Err(e) = config {
+        eprintln!("Could not read file {}: {}", goto_key_paths_file_path(), e);
         return -1;
     }
 
-    // Iterate over the lines and exclude the line to be removed
-    for line in io::BufReader::new(&file).lines() {
-        // Write non-matching lines to the buffer
-        if let Ok(ref ip) = line {
-            let key_path_pair = KeyPath::from_entry(&ip);
-            if key_path_pair.is_valid() && key_path_pair.key() != key {
-                buffer.extend(line.unwrap().bytes());
-                buffer.push(b'\n');
-            }
+    match config.unwrap().remove_keypath(key) {
+        Err(e) => {
+            eprintln!("could not remove key: {}", key);
+            eprintln!("{}", e);
+            return -1;
+        } Ok (_) => {
+
         }
-    }
-
-    // Truncate the file to the current position (i.e., remove the remaining contents)
-    let seek_current = file.seek(SeekFrom::Start(0)).unwrap();
-    if let Err(error) = file.set_len(seek_current) {
-        eprintln!("Error occured: {}", error);
-        return -1;
-    }
-
-    // Write the modified contents back to the file
-    if let Err(error) = file.write_all(&buffer) {
-        eprintln!("Error occured: {}", error);
-        return -1;
     }
 
     return 0;
 }
 
 fn add_key_path(key: &String, path: &String) -> i32 {
-    // See if key already exists
-    match get_file_reader_for_file(&goto_key_paths_file_path()) {
-        Err(e) => {
-            eprintln!("Could not read file {}: {}", goto_key_paths_file_path(), e);
-            return -1;
-        } Ok(reader) => {
-            for line in reader.lines() {
-                if let Ok(ip) = line {
-                    // key|path
-                    let key_path_pair = KeyPath::from_entry(&ip);
-                    if !key_path_pair.is_valid() && key_path_pair.key() == key {
-                        eprintln!("key ({key}) already exists");
-                        return -1;
-                    }
-                }
+    let key_paths_file_path = goto_key_paths_file_path();
+    let config = Config::new(&key_paths_file_path);
+    if let Err(e) = config {
+        eprintln!("Could not read file {}: {}", goto_key_paths_file_path(), e);
+        return -1;
+    } else {
+        let conf = config.as_ref().unwrap();
+        for key_path_pair in conf.entries() {
+            if key_path_pair.is_valid() && key_path_pair.key() == key {
+                eprintln!("key ({key}) already exists");
+                return -1;
             }
         }
     }
-
-    // Write new key and path pair
-    let mut file_writer = OpenOptions::new().create(true).write(true).append(true).open(&goto_key_paths_file_path()).unwrap();
-
-    // Expand the input path
+    
+    // create key path
     let expanded_path = expand_path(&path);
-
     let kp = KeyPath::new(&key, &expanded_path);
-
     if !kp.is_valid() {
         eprintln!("invalid key path pair");
         return -1;
     }
-
-    // write
-    if let Err(error) = writeln!(file_writer, "{}", kp.entry()) {
-        eprintln!("Error occurred writing line: {}", error);
-        return -1;
-    }
-
-    // Make sure the writer flushed all data
-    if let Err(error) = file_writer.flush() {
-        eprintln!("Could not flush file: {}", error);
-        return -1;
+  
+    // write new keypath to config
+    match config.unwrap().enter_keypath(kp) {
+        Err(e) => {
+            eprintln!("experienced an error entry new key path: {}", e);
+        } Ok(_) => {}
     }
 
     return 0;
 }
 
-fn get_file_reader_for_file(path: &str) -> Result<BufReader<File>, io::Error> {
-    let file = File::open(path)?;
-    let reader = BufReader::new(file);
-    Ok(reader)
-}
 
 fn goto_utils_path() -> String {
     let mut res = PathBuf::from(home::home_dir().unwrap());
@@ -317,16 +258,35 @@ fn expand_path(path: &str) -> String {
     return canonicalize(path).unwrap().into_os_string().into_string().unwrap();
 }
 
+fn path_exists(path: &str) -> bool {
+    if let Ok(_) = fs::metadata(path) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
     use std::fs;
+    use std::fs::File;
+    use std::io::Write;
 
-    fn setup() {
+    /**
+     * Creates a keypaths with one entry
+     */
+    pub fn setup() {
         // create the test env
         let mut path = goto_utils_path();
-        let mut result = fs::create_dir(&path);
-        assert!(result.is_ok());
+
+        if path_exists(&path) {
+            let result = fs::remove_dir_all(&path);
+            assert!(result.is_ok());
+        }
+
+        let result = fs::create_dir(&path);
+        assert!(result.is_ok(), "couldn't create {}", path);
        
         // make sure it is created
         let meta = fs::metadata(&path);
@@ -341,11 +301,11 @@ mod tests {
         // write test data
         let home_dir = PathBuf::from(home::home_dir().unwrap());
         let line = "home|".to_owned() + &home_dir.to_string_lossy().to_string();
-        result = file.expect("could not write to file").write_all(line.as_bytes());
+        let result = file.expect("could not write to file").write_all(line.as_bytes());
         assert!(result.is_ok());
     }
 
-    fn teardown() {
+    pub fn teardown() {
         let path = goto_utils_path();
         let result = fs::remove_dir_all(path);
         assert!(result.is_ok());
@@ -370,23 +330,6 @@ mod tests {
     #[test]
     fn goto_util_keypath_file_path_not_empty() {
         assert!(!goto_key_paths_file_path().is_empty());
-    }
-
-    #[test]
-    fn valid_file_reader() {
-        setup();
-        let reader = get_file_reader_for_file(&goto_key_paths_file_path());
-        assert!(reader.is_ok());
-        assert!(reader.unwrap().lines().count() == 1, "we are expecting only one line in this test case");
-        teardown();
-    }
-
-    fn path_exists(path: &str) -> bool {
-        if let Ok(_) = fs::metadata(path) {
-            return true;
-        } else {
-            return false;
-        }
     }
 
     #[test]
