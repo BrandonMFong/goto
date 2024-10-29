ffaa99f5ecbe2be9dee0f6a40d6d285eb63fb6cb
Brandon Fong <fong.m.brandon97@gmail.com>
2024-10-29 16:32:09 -0700

    trying to check shell

--- scripts/gotoutils.sh
+++ scripts/gotoutils.sh
@@ -58,12 +58,14 @@ function goto() {
 	return $error;
 }
 
-# https://keyholesoftware.com/2022/07/18/adding-autocompletion-to-bash-scripts/
-function __goto_completion() {
-	if [ $COMP_CWORD -eq 1 ]; then 
-		cur=${COMP_WORDS[COMP_CWORD]}
-		COMPREPLY=( $($GOTO_UTILS_TOOL --show-suggested-keys $cur) )
-	fi
-}
-complete -F __goto_completion goto
+if [ "$0" == "-bash" ]; then 
+	# https://keyholesoftware.com/2022/07/18/adding-autocompletion-to-bash-scripts/
+	function __goto_completion() {
+		if [ $COMP_CWORD -eq 1 ]; then 
+			cur=${COMP_WORDS[COMP_CWORD]}
+			COMPREPLY=( $($GOTO_UTILS_TOOL --show-suggested-keys $cur) )
+		fi
+	}
+	complete -F __goto_completion goto
+fi
 
