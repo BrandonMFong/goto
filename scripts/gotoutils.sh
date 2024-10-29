5e7f94c6d05d2d2fb6b3a4a5ed4fddd782eef8a6
brando <fong.m.brandon97@gmail.com>
2024-10-29 13:52:04 -0700

    cleaning up functions we will not use

--- scripts/gotoutils.sh
+++ scripts/gotoutils.sh
@@ -44,60 +44,12 @@ function goto() {
 	return $error;
 }
 
-# gets path from keypaths file using key pair then cds to it
-function _goto() {
-	p=$($GOTO_UTILS_TOOL getpath $1);
-	error=$?;
-	if [ $error -eq 0 ]; then 
-		cd $p;
-	fi
-}
-
-# cds into the last previous path used in goto()
-function goto-prev() {
-	p=$($GOTO_UTILS_TOOL getpath-prev);
-	error=$?;
-	if [ $error -eq 0 ]; then 
-		cd $p;
-	else
-		__goto-print-error "history empty";
-	fi
-
-}
-
 # https://keyholesoftware.com/2022/07/18/adding-autocompletion-to-bash-scripts/
 function __goto_completion() {
 	if [ $COMP_CWORD -eq 1 ]; then 
 		cur=${COMP_WORDS[COMP_CWORD]}
-		COMPREPLY=( $(./bin/release/gototool getsugkeys $cur) )
+		COMPREPLY=( $($GOTO_UTILS_TOOL --show-suggested-keys $cur) )
 	fi
 }
 complete -F __goto_completion goto
 
-function goto-add() {
-	if [ $# -ne 2 ]; then
-		__goto-print-error "goto-add <key> <path>";
-	else
-		$GOTO_UTILS_TOOL add $1 $2;
-	fi
-}
-
-function goto-remove() {
-	$GOTO_UTILS_TOOL rm $1;
-}
-
-function goto-showkeys() {
-	$GOTO_UTILS_TOOL getkeys $1;
-}
-
-function __goto-print-error() {
-	echo "goto: $@" 1>&2;
-}
-
-function goto-showall() {
-	$GOTO_UTILS_TOOL getallpairs;
-}
-
-function goto-version() {
-	$GOTO_UTILS_TOOL version;
-}
